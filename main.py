5e1a8833ad177ba8dcdc09cba01e6553e6413537
ngsneto@gmail.com <ngsneto@gmail.com>
2022-07-27 11:55:19 -0300

    add

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#conteúdo programático
+
+Cap01 - Introdução
+Cap02 - Variáveis, Tipos e Estruturas de Dados
+Cap03- Loops,Condicionais,Métodos   e Funções
+Cap04- Tratamento de arquivos,Módulos  e Pacotes
+Cap05-  Orientação a objetivos
+Cap06- Manipulando Banco de dados em Python
+Cap07- Desafio DSA
+Cap08-Módulos Python para  Análise   de Dados
+Cap09- Introdução a análise de dados em Python
+Cap10 - Introdução ao TensorFlow
+Cap11 - Introdução a Machine Learning com Python
+Cap12 - Bônus -  Introdução a Deep Learning
+Cap13 - Desenvolvimento web
+Cap14 -  Introdução ao Web Scraping
+CAP15 - AVALIAÇÃO  FINAL E CERTIFICADO
