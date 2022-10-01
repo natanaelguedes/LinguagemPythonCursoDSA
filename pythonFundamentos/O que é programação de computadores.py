cab5aab459d122ef1d9dcc9b207e5b9a07937df3
Natanael <ngsneto@gmail.com>
2022-10-01 12:26:20 -0300

    add

---
+++ pythonFundamentos/O que é programação de computadores.py
@@ -0,0 +1,5 @@
+#Programação é o processo de analisar uma
+#tarefa,dividi-la em pedaços ou etapas e escrever uma
+#sequencia de instruções que o computador possa executar
+#python , Data science, Inteligencia Artificial (AI)
+
