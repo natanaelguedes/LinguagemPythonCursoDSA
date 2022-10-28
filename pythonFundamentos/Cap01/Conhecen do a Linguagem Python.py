471d151343dc70852fae7e2781fee21e5fd9642a
Natanael <ngsneto@gmail.com>
2022-10-27 21:43:31 -0300

    add

---
+++ pythonFundamentos/Cap01/Conhecen do a Linguagem Python.py
@@ -0,0 +1,17 @@
+#Python Fundamentos para Análise de Dados
+#É fácil de usar e poderosa e de alto nível
+#É uma abordagem simples e orientada a objetos
+#possui uma sintaxe elegante e tripagem dinâmica
+#Possui desenvolvimento rápido  e linguagem de programação que pode fazer tudo o que você precisar
+
+#o que pode fazer:
+#-Coleta de   dados, engenharia de dados, web scraping python. inteligência artificial, execução de  aplicativos web.
+
+#Sobre a Linguagem Python:
+#-lançada em 1991, na Holanda, por Guido Van  Rossum
+#Linguagem intepretada
+#Orientada a Objetos
+#Portável (a linguagem roda em qualquer  plataforma)
+#Comunidade Ativa
+#O  interpretador python está disponivel em www.python.org nas três plataformas: MacOS,Windows, Linux
+#Contém distribuições inter-inter para diversos módulos de terceiros totalmente gratuito, além de ferramentas e documetação adicional.
