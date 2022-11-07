84608dc34c45161b178eaaba6f9da39e17b4ddc0
Natanael <ngsneto@gmail.com>
2022-11-06 22:28:16 -0300

    add

---
+++ pythonFundamentos/CAP02- variáveis e tipos de estruturas de dados/INTRODUÇÃO.py
@@ -0,0 +1,14 @@
+#Variáveis,
+#Tipos e estruturas de dados
+
+#Python Fundamentos
+#Números e operações matemáticas
+#Variáveis e operadores
+#Strings
+#Estruturas de dados:
+#-Listas, Dicionários e Tuplas
+
+#No fim desse capitulos:
+#Quiz
+#Exercicios
+#Lab01
