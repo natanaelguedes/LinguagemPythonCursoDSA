8a6d109b99113be4a52228e4c49173e40c492033
Natanael <ngsneto@gmail.com>
2022-11-06 22:15:42 -0300

    add

--- pythonFundamentos/Cap01/Utilizando JupyterNotebook/Para que serve o JupyterNotebook.py
+++ pythonFundamentos/Cap01/Utilizando JupyterNotebook/Para que serve o JupyterNotebook.py
@@ -1,1 +1,21 @@
 # O JupyterNotebook serve para fazer documentações  e manual de instruções
+# O Jupyter Notebook é uma ferramenta de Literate Computing, extremamente eficientes,
+# pois permitem unir código e texto. Assim cada funcionalidade pode ser explicada detalhadamente.
+# Você também pode gerar gráficos “vivos” gerados em tempo real dentro da ferramenta.
+#
+# O Jupyter Notebook é revolucionário, justamente por unir texto e código.
+# Tenho usado muito para mapear e fazer anotações da minha pesquisa do mestrado e
+# nenhuma ferramenta foi mais simples e eficiente.
+#
+# Utilizando o Jupyter Notebook é possível descrever a questão da pesquisa, a abordagem do projeto,
+# narrar as etapas importantes na análise. Sempre mesclando explicações e o código de programação.
+# Esses documentos podem ser construídos de forma dinâmica, escrevendo porções do código da narrativa e do software,
+# visualizando os resultados no relatório e continuando a análise com narração e código adicionais
+# ou resumindo os resultados.
+#
+# Um documento completo de programação também pode ser gerado com todos os componentes críticos do projeto:
+# a descrição da proposta, o código de programação usado para realizá-lo, os resultados e uma discussão.
+#
+# O Jupyter também tem sido muito utilizado em cursos e workshops de programação,
+# por permitir que os participantes alterem parâmetros do código e
+# rodem ali na própria máquina e já observem o resultado em tempo real
