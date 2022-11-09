304dcacfdaf8908c175239e157a674dc239c08c4
Natanael <ngsneto@gmail.com>
2022-11-09 20:07:20 -0300

    add

---
+++ pythonFundamentos/CAP02- variáveis e tipos de estruturas de dados/PythonFundamentos .py
@@ -0,0 +1,37 @@
+#Python é uma linguagem interpretada,
+#clara, de fácil leitura e bastante expressiva
+#o código fonte é traduzido em bytecode que
+# é um formato binários com instruções para o
+#intepretador e  é multiplataforma.
+
+#Código fonte (m.py) + Byte(m.pyc) = Runtime(PVM)
+#Basicamente, existem 3 modos de executar programas em PyTHON:
+#Modo shell. Modo script (Arquivos com extensão .py)
+#Modo interativo  (Jupyter Notebook)
+
+#indentação (tecla tab, ou 4 clique na barra de espaço)
+
+print(nivel 1)#primeiro nível hierárquico
+if(True):
+    print(nivel 2) #segundo nível hierárquico
+
+    #indentação  faz parte da sintaxe em Python
+    #Block0
+    #Header-line
+               #Block1
+               #Header-line
+                         #Block2
+               #Block0
+     #Seja persistente use tab ou espaço não misture os dois
+    #COMENTÁRIOS em Python
+    #começam com o caracter #  ou 3 aspas duplas """..."""
+    #ISSO É UM COMENTÁRIO EM UMA ÚNICA LINHA
+    """ISSO É  UM COMENTÁRIO EM MAIS  DE UMA LINHA"""
+
+    #DICAS
+    #Clareza é importante. Mantenha seu código  limpo e organizado
+    #CÓDIGO ESPARSO É MELHOR QUE CÓDIGO DENSO.
+    #SEMPRE DOCUMENTE SEU CÓDIGO
+    #SIGA OS PADRÕES NÃO PARA CR IAR COMPLEXIDADE, MAS PARA MANTER REGRAS
+    #$OS ERROS NUNCA SERÃO SILENCIOSOS, A MENOS QUE PROPOSITALMENTE.
+    #NÃO SE SINTA OBRIGADO A CRIAR CLASSES  SEM UMA BOA RAZÃO
